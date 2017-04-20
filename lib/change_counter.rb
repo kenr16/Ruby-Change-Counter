770f23e47b7a8c70cf33b0dd8b83ae0fd730e9c7
Ken Rutan and Susha Dore <susha@>
2017-04-20 10:42:08 -0700

    add capybara testing specs and passed all specs.

--- lib/change_counter.rb
+++ lib/change_counter.rb
@@ -5,7 +5,7 @@ class Fixnum
     dimes = 0
     nickels = 0
     pennies = 0
-    availible_quarters = 3
+    availible_quarters = 2
     availible_dimes = 2
     availible_nickels = 1
 
@@ -32,15 +32,25 @@ class Fixnum
       end
       index += 1
     end
+
+    output = "#{quarters} quarters, #{dimes} dimes, #{nickels} nickels and #{pennies} pennies"
+
+    if availible_quarters == 0 || availible_dimes == 0 || availible_nickels == 0
+      output.concat(" Sorry, we ran out of ")
+    end
     if availible_quarters == 0
-      puts "Sorry, we ran out of quarters."
+      output.concat("quarters")
     end
     if availible_dimes == 0
-      puts "Sorry, we ran out of dimes."
+      output.concat(" dimes")
     end
     if availible_nickels == 0
-      puts "Sorry, we ran out of nickels."
+      output.concat(" nickels")
     end
-    output = "#{quarters} quarters, #{dimes} dimes, #{nickels} nickels and #{pennies} pennies"
+    if availible_quarters == 0 || availible_dimes == 0 || availible_nickels == 0
+      output.concat(".")
+    end
+
+    output
   end
 end
